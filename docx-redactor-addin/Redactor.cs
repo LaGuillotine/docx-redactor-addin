aac7bd8edcf255d1d944bbd36462ff997ff890b1
Philippe Barias <philippe.barias@gmail.com>
2020-03-07 22:33:23 +0100

    Refactoring and fixed the redactor going stale

--- docx-redactor-addin/Redactor.cs
+++ docx-redactor-addin/Redactor.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml;
 using DocumentFormat.OpenXml;
 using Microsoft.Office.Interop.Word;
 using DocumentFormat.OpenXml.Wordprocessing;
@@ -11,74 +8,46 @@ using Document = Microsoft.Office.Interop.Word.Document;
 namespace docx_redactor_addin {
     public class Redactor {
         private readonly Document _document;
-        private readonly Dictionary<HighlightColorValues, List<Run>> _color2Run = new Dictionary<HighlightColorValues, List<Run>>();
-
-        private XmlDocument _xmlDocument;
-        private XmlNode _documentNode;
 
         public Redactor(Document document) {
             _document = document;
-            BuildColorMap();
         }
 
         public bool KeepHighlight { get; set; } = false;
 
         public string Replacement { get; set; } = "redacted";
 
-        private void BuildColorMap() {
-            string xml = _document.Content.WordOpenXML;
-            string documentXml = ExtractDocumentXmlElement(xml);
+        public void RedactLikeRange(Range range) {
+            HighlightColorValues color = ColorIndex2Value(range.HighlightColorIndex);
 
-            OpenXmlDocument doc = new OpenXmlDocument(documentXml);
+            OpenXmlIntegrator integrator = new OpenXmlIntegrator(_document);
+            OpenXmlDocument document = integrator.ExtractDocumentElement();
+            
+            IEnumerable<Run> runs = GetRunsWithColor(document, color);
+            foreach (Run currentRun in runs) PerformReplacement(currentRun);
 
-            void addRun(Run run) {
-                if (run.RunProperties?.Highlight == null) return;
-
-                HighlightColorValues color = run.RunProperties.Highlight.Val.Value;
-                bool exists = _color2Run.TryGetValue(color, out List<Run> runs);
-
-                if (!exists) {
-                    runs = new List<Run>();
-                    _color2Run[color] = runs;
-                }
-
-                runs.Add(run);
-            }
-
-            foreach (Run run in CollectRuns(doc)) {
-                addRun(run);
-            }
+            string xml = integrator.IntegrateAndReturnOpenXml(document);
+            _document.Content.InsertXML(xml);
         }
 
-        private string ExtractDocumentXmlElement(string xml) {
-            using (TextReader tr = new StringReader(xml)) {
-                _xmlDocument = new XmlDocument();
-                _xmlDocument.Load(tr);
+        private static IEnumerable<Run> GetRunsWithColor(OpenXmlElement document, HighlightColorValues color) {
+            List<Run> runs = new List<Run>();
+
+            foreach (Run run in CollectRuns(document)) {
+                if (run.RunProperties?.Highlight == null) continue;
+
+                HighlightColorValues highlightColor = run.RunProperties.Highlight.Val.Value;
+                if (highlightColor == color) runs.Add(run);
             }
 
-            XmlNode searchDocument(XmlNode node) {
-                if (node.LocalName == "document") return node;
+            return runs;
+        }
 
-                XmlNode searchedNode = null;
-                foreach (XmlNode child in node.ChildNodes) {
-                    XmlNode searchResult = searchDocument(child);
+        private void PerformReplacement(Run run) {
+            run.InnerXml = Replacement;
 
-                    if (searchResult?.LocalName != "document") continue;
-                    searchedNode = searchResult;
-                    break;
-                }
-
-                return searchedNode;
-            }
-
-
-            XmlNode currentRoot = _xmlDocument.FirstChild;
-            while (_documentNode == null && currentRoot != null) {
-                _documentNode = searchDocument(currentRoot);
-                currentRoot = currentRoot.NextSibling;
-            }
-
-            return _documentNode != null ? _documentNode.OuterXml : string.Empty;
+            if (KeepHighlight) return;
+            run.RunProperties?.Highlight?.Remove();
         }
 
         private static IEnumerable<Run> CollectRuns(OpenXmlElement document) {
@@ -97,47 +66,6 @@ namespace docx_redactor_addin {
             collect(document);
 
             return runs;
-        }
-
-        public void RedactLikeRange(Range range) {
-            HighlightColorValues color = ColorIndex2Value(range.HighlightColorIndex);
-            if (!_color2Run.TryGetValue(color, out List<Run> runs)) return;
-
-            foreach (Run currentRun in runs) {
-                PerformReplacement(currentRun);
-            }
-
-            if (!KeepHighlight) _color2Run.Remove(color);
-            
-            ApplyChanges();
-        }
-
-        private void PerformReplacement(Run run) {
-            run.InnerXml = Replacement;
-
-            if (KeepHighlight) return;
-            run.RunProperties?.Highlight?.Remove();
-        }
-
-        private void ApplyChanges() {
-            OpenXmlElement findRoot() {
-                Run someRun = _color2Run.Values.First().First();
-
-                OpenXmlElement node = someRun;
-                do {
-                    node = node.Parent;
-                } while (node.Parent != null);
-
-                return node;
-            }
-
-            OpenXmlElement root = findRoot();
-
-            if (_documentNode.ParentNode == null) return;
-            _documentNode.ParentNode.InnerXml = root.OuterXml;
-
-            string xml = _xmlDocument.OuterXml;
-            _document.Content.InsertXML(xml);
         }
 
         private static HighlightColorValues ColorIndex2Value(WdColorIndex colorIndex) {
